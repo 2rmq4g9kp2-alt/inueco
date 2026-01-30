9d1a14a5ebf22697c4795facd54377677cc31362
2rmq4g9kp2-alt <2rmq4g9kp2@privaterelay.appleid.com>
2026-01-30 11:57:49 +0500

    бузуев

---
+++ Управление качеством и надежность информационных систем/lesson 1,2/func_input.py
@@ -0,0 +1,22 @@
+number = input()
+number = int(number)
+print(number)
+print(type(number))
+
+"""
+не возвращает значение
+print()
+
+возвращает значение:
+type()
+input()
+int()
+float()
+str()
+bool()
+list()
+dict()
+tuple()
+set()
+frozenset()
+"""
